04580da72905014262746a5d3f5cadbe29e33eb7
IshmamR <ishmam785@gmail.com>
2022-02-05 20:06:54 +0600

    max pool size

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import logger from "../utils/logger";
 
 const options = {
-  maxPoolSize: 10, // Maintain up to 10 socket connections
+  maxPoolSize: 100, // Maintain up to 100 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   // family: 4, // Use IPv4, skip trying IPv6
